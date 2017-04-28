c334306ac4a4da824a8fedfa4416c191713354ec
Songsterr99 <mr.songsterr@mail.ru>
2017-04-28 17:27:53 +0300

    begininng

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	public GameObject inventory;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.I)) {
+			if (inventory.activeSelf)
+				inventory.SetActive (false);
+			else
+				inventory.SetActive (true);
+		}
+	}
+}
