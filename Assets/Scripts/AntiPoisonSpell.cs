2b4a3e98e6587e6fd6807bbac0a58fa938a4dbff
Foored <Foored1999@gmail.com>
2017-05-29 17:12:15 +0300

    3d spell

---
+++ Assets/Scripts/AntiPoisonSpell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiPoisonSpell : MonoBehaviour, Spell {
+
+    int artID = 9;
+    int manacost = 100;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && GameState.ArtID == artID)
+            Cast();
+    }
+
+    public void Cast()
+    {
+        GameState.Player.mana -= manacost;
+        if (GameState.Player.state == states.отравлен)
+            GameState.Player.state = states.здоров;
+    }
+}
